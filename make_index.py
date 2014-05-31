ff8b54437ae864414a597777e85483144aab6e66
Joe Germuska <joe@germuska.com>
2014-05-31 11:40:53 -0500

    make the index-maker slightly more reusable

--- make_index.py
+++ make_index.py
@@ -3,9 +3,9 @@ import os.path
 
 HTML_HEAD = """
 <html>
-<head><title>%(title)s</title></head>
+<head><title>ACS Data Aggregated by %(title)s</title></head>
 <body>
-<h1>%(title)s</h1>
+<h1>ACS Data Aggregated by %(title)s</h1>
 <p>Aggregated data from the 2008-2012 American Community Survey. (<a href="../index.html">huh?</a>)
 </p>
 """
@@ -21,7 +21,7 @@ def write_index(datadir,title):
             outfile.write(HTML_HEAD % {'title': title})
             for line in infile:
                 (table_code, title) = re.split('\s+', line, 1)
-                subs = {'table_code': table_code, 'title': title}
+                subs = {'table_code': table_code.lower(), 'title': title}
                 outfile.write("""
 [<a href="%(table_code)s_moe.csv">data</a>] [<a href="%(table_code)s.txt">columns</a>] <strong>%(table_code)s</strong>: %(title)s<br>
 """ % subs)
@@ -29,3 +29,10 @@ def write_index(datadir,title):
 
             outfile.write(HTML_FOOT)
             
+if __name__ == '__main__':
+    import sys
+    datadir, title = sys.argv[1:3]
+    if not datadir or not title:
+        raise Exception("Specify a data directory and title on the commandline")
+    write_index(datadir, title)
+
